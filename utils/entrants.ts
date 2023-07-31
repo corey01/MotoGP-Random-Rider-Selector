891d76f3eef205083e2699c3284fa76859d5429a
Corey O'Beirne <corey.obeirne@gmail.com>
2023-07-31 22:49:25 +0100

    remove console.log()

--- utils/entrants.ts
+++ utils/entrants.ts
@@ -2,14 +2,7 @@ export const defaultEntrants = ["Mum", "Dad", "Corey", "Norma"];
 
 const acceptedEntrants = [...defaultEntrants, "Danii", "Duffy", "Matt"];
 
-// export const isInDefaultEntrants = (thisEntrant: string) => {
-//   const lookup = defaultEntrants.find((ent) => ent === thisEntrant);
-
-//   return !!lookup || false;
-// };
-
 export const isInAcceptedEntrants = (thisEntrant: string) => {
-  console.log(thisEntrant, acceptedEntrants);
   const lookup = acceptedEntrants.find(
     (ent) => ent.toLowerCase() === thisEntrant.toLowerCase()
   );
@@ -18,7 +11,6 @@ export const isInAcceptedEntrants = (thisEntrant: string) => {
 };
 
 export const getEntrantImage = (value: string) => {
-  // console.log(value);
   if (isInAcceptedEntrants(value)) {
     return require(`/public/entrants/${value.toLowerCase()}.jpg?resize&size=200&webp`);
   }
