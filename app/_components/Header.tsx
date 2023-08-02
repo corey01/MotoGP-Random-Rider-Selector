70e3372f3373087d93774ab4d9ea978f89b3e028
Corey O'Beirne <corey.obeirne@gmail.com>
2023-08-02 21:31:14 +0100

    some minor style changes for mobile layout

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,9 +1,20 @@
+import Link from "next/link";
 import { motoGP } from "../fonts";
 import style from "./Header.module.scss";
 
 const Header = () => {
   return (
     <div className={style.header}>
+      <nav className={style.headerNav}>
+        <ul>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/calendar">Calendar</Link>
+          </li>
+        </ul>
+      </nav>
       <h1 className={motoGP.className}> MotoGP Random Rider Selector</h1>
     </div>
   );
