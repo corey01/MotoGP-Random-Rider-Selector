4013e8a4e5a0441af5287b8d2bd610239b28061c
Corey O'Beirne <corey.obeirne@gmail.com>
2023-08-03 11:17:20 +0100

    Removed unused file

--- utils/createQueryString.ts
+++
@@ -1,8 +0,0 @@
-const createQueryString = 
-  (name: string, value: string) => {
-    const params = new URLSearchParams(searchParams)
-    params.set(name, value)
-
-    return params.toString()
-  }
-)
