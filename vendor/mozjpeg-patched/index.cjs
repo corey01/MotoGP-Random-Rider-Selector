cd60ddb275ad43f272e834ab4b9de946a1ac83d9
Corey O'Beirne <corey.obeirne@gmail.com>
2025-09-17 21:21:54 +0100

    chore: add cjs entry for mozjpeg

---
+++ vendor/mozjpeg-patched/index.cjs
@@ -0,0 +1,20 @@
+'use strict';
+
+const fs = require('node:fs');
+const path = require('node:path');
+const process = require('node:process');
+const BinWrapper = require('bin-wrapper');
+
+const pkg = JSON.parse(
+  fs.readFileSync(path.join(__dirname, 'package.json'), 'utf8')
+);
+const url = `https://raw.githubusercontent.com/imagemin/mozjpeg-bin/v${pkg.version}/vendor/`;
+
+const binWrapper = new BinWrapper()
+  .src(`${url}macos/cjpeg`, 'darwin')
+  .src(`${url}linux/cjpeg`, 'linux')
+  .src(`${url}win/cjpeg.exe`, 'win32')
+  .dest(path.join(__dirname, 'vendor'))
+  .use(process.platform === 'win32' ? 'cjpeg.exe' : 'cjpeg');
+
+module.exports = binWrapper.path();
