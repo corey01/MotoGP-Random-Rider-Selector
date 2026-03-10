c8c5207c92a62399facc3a44084b6f12b409fff5
Corey O'Beirne <corey.obeirne@gmail.com>
2026-03-10 20:44:21 +0000

    remove unused dependancies and cleanup build script

--- next.config.js
+++ next.config.js
@@ -1,29 +1,6 @@
 /** @type {import('next').NextConfig} */
 
-// const withOptimizedImages = require("next-optimized-images");
 
-// module.exports = withOptimizedImages({
-//   /* config for next-optimized-images */
-
-//   // your config for other plugins or the general next.js here...
-
-//   output: "export",
-//   images: {
-//     disableStaticImages: true,
-//     unoptimized: true,
-//   },
-// });
-
-const withOptimisedImages = require("@hashicorp/next-optimized-images");
-
-module.exports = withOptimisedImages({
+module.exports = {
   output: "export",
-  images: {
-    disableStaticImages: true,
-    unoptimized: true,
-  },
-
-  responsive: {
-    adapter: require("responsive-loader/sharp"),
-  },
-});
+};
